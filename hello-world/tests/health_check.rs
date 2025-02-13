14baf1e3d6d93f3d65f6c9772e9424dd66cd24e2
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-13 14:10:04 +0400

    bezi deyisiklikler

--- hello-world/tests/health_check.rs
+++ hello-world/tests/health_check.rs
@@ -1,8 +1,6 @@
-
 #[tokio::test]
 async fn health_check_works() {
-   spawn_app().await.expect("Failed to spawn our app");
-
+    spawn_app();
 
     let client = reqwest::Client::new();
 
@@ -16,6 +14,8 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run("127.0.0.1:8000").expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
 }
