15c12118925e2281f4ebefe91600546ece9ec7cf
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-13 16:07:31 +0400

    iki test elave etdik biri isledi biri ise pase olinmadi

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -3,13 +3,21 @@ use std::net::TcpListener;
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> impl Responder {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::run;
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
     println!("{:?}", listener);
     run(listener)?.await
 }
