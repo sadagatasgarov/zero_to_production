4e919a6c5778ba83ed397fea50dae0d47b2b7e5c
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-18 16:03:32 +0400

    mainde deyisiklikler funksiyaalara bolme]

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,28 +1,38 @@
 use std::net::TcpListener;
-
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-
 use tracing::subscriber::set_global_default;
+
+pub fn get_subscriber(
+    name: String,
+    env_filter: String
+) -> impl Subscriber +Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync ) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to get subscriber");
+}
+
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     //env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to get subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
