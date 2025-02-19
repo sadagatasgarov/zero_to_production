6ea869b7251f21c7b542a20d86b1b0f4c9566787
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-19 17:19:48 +0400

    buradaki Sink demeli generic imis

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -8,7 +8,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     //env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(),std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
