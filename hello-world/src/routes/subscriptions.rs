674e245749fae0fbc7e0f5742af62217ef39a6b6
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-24 01:01:11 +0400

    refactor etdik new subcriber try into ile formdan cevirdik

--- hello-world/src/routes/subscriptions.rs
+++ hello-world/src/routes/subscriptions.rs
@@ -7,6 +7,15 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
 pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
     let name = SubscriberName::parse(form.name)?;
     let email = SubscriberEmail::parse(form.email)?;
@@ -25,7 +34,7 @@ pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
     )
 ]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let new_subscriber = match parse_subscriber(form.0) {
+    let new_subscriber = match form.0.try_into() {
         Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
