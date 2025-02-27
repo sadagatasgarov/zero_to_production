aebcb26222df21d9c20d7b114574f26ec39888f5
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-27 23:16:04 +0400

    green test

--- hello-world/src/routes/subscriptions.rs
+++ hello-world/src/routes/subscriptions.rs
@@ -43,12 +43,34 @@ pub async fn subscribe(
     }
     // Send a (useless) email to the new subscriber.
     // We are ignoring email delivery errors for now.
+    // if email_client
+    //     .send_email(
+    //         new_subscriber.email,
+    //         "Welcome!",
+    //         "Welcome to our newsletter!",
+    //         "Welcome to our newsletter!",
+    //     )
+    //     .await
+    //     .is_err()
+    // {
+    //     return HttpResponse::InternalServerError().finish();
+    // }
+    // HttpResponse::Ok().finish();
+
+    let confirmation_link = "https://there-is-no-such-domain.com/subscriptions/confirm";
     if email_client
         .send_email(
             new_subscriber.email,
             "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
+            &format!(
+                "Welcome to our newsletter!<br />\
+    Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            ),
+            &format!(
+                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+                confirmation_link
+            ),
         )
         .await
         .is_err()
