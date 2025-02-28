522ce169e3f621dd2e6ca703ac9959abe03592f0
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-02-28 09:39:06 +0400

    subscribeni dogrulamak icin ayri funksiya

--- hello-world/tests/api/subscriptions.rs
+++ hello-world/tests/api/subscriptions.rs
@@ -17,13 +17,27 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=Sadagat%20Asgarov&email=sadagatasgarov%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+    // Act
+    app.post_subscriptions(body.into()).await;
+    // Assert
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
-        .expect("Failed to fetch saved subscriptions");
-
+        .expect("Failed to fetch saved subscription.");
     assert_eq!(saved.email, "sadagatasgarov@gmail.com");
+    assert_eq!(saved.name, "Sadagat Asgarov");
 }
 
 #[tokio::test]
