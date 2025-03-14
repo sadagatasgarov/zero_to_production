ae28a23e936bab1b7f13c2462f36ceafaead919b
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-03-14 15:39:15 +0400

    dashborada yonlendirme testi ugursuz oldu sessionlar hazirdeyil

--- hello-world/tests/api/helpers.rs
+++ hello-world/tests/api/helpers.rs
@@ -55,6 +55,17 @@ impl TestApp {
             .unwrap()
     }
 
+    pub async fn get_admin_dashboard_html(&self) -> String {
+        self.api_client
+            .get(&format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+            .text()
+            .await
+            .unwrap()
+    }
+
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         self.api_client
             .post(&format!("{}/subscriptions", &self.address))
